a0805d1bf46abdbba7fd75001619fcc9394e59c3
Asad <asadahsan2245@gmail.com>
2026-03-03 20:33:21 +0530

    second commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from utils.video_utils import read_video, save_video
+
+def main():
+    # read vid
+    video_path = "D:\\Projects\\End-to-End\\football-analysis-cv\\input_videos\\08fd33_4.mp4"
+    frames = read_video(video_path)
+
+    # save video
+    output_video_path = "D:\\Projects\\End-to-End\\football-analysis-cv\\output_videos\\output_video.avi"
+    save_video(frames, output_video_path)
+
+if __name__ == "__main__":
+    main()

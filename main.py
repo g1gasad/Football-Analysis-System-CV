6eddb41a9c02191e458e2b4235a8d689f9746da4
Asad <asadahsan2245@gmail.com>
2026-03-10 23:19:56 +0530

    fixed the project

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from trackers import tracker
 
 def main():
     # read vid
-    video_path = "D:\\Projects\\End-to-End\\football-analysis-cv\\input_videos\\08fd33_4.mp4"
+    video_path = "D:\\Projects\\End-to-End\\Football-Analysis-System\\input_videos\\08fd33_4.mp4"
     frames = read_video(video_path)
 
     # init tracker
-    tracker = Tracker("models/best.pt")
-    tracks = tracker.get_object_tracks(frames)
+    tracker_instance = tracker.Trackers("models/best.pt")
+    tracks = tracker_instance.get_object_tracks(frames)
 
     # save video
-    output_video_path = "D:\\Projects\\End-to-End\\football-analysis-cv\\output_videos\\output_video.avi"
+    output_video_path = "D:\\Projects\\End-to-End\\Football-Analysis-System\\output_videos\\output_video.avi"
     save_video(frames, output_video_path)
 
 if __name__ == "__main__":

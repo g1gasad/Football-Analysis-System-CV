6a2f30caf048591bb10da0b74a8607fe3d938065
Asad <asadahsan2245@gmail.com>
2026-03-25 22:47:32 +0530

    started camera movement estimator module

---
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -0,0 +1,53 @@
+import pickle
+import cv2
+import numpy as np
+
+class CameraMovementEstimator:
+    def __init__(self, frame):
+
+        self.lk_params = dict(
+            winSize = (15, 15),
+            maxLevel = 2,
+            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+        )
+
+        first_frame_grayscale = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+        mask_features = np.zeros_like(first_frame_grayscale)
+        mask_features[:, 0:20]=1
+        mask_features[:, 900:1050]=1
+
+        self.features = dict(
+            maxCorners=100,
+            qualityLevel=.3,
+            minDistance=3,
+            blockSize=7,
+            mask=mask_features
+        )
+
+
+
+    def get_camera_movement(self, frames, read_from_stub=False, stub_path=None):
+        # read the stub
+
+        camera_movement = [[0,0] * len(frames)]
+
+        old_gray = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
+        old_features = cv2.goodFeaturesToTrack(old_gray, **self.features)
+
+        for frame_num in range(1, len(frames)):
+            frame_gray = cv2.cvtColor(frames[frame_num], cv2.COLOR_BGR2GRAY)
+            new_features, _, _ = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, old_features, None,
+                                                    **self.lk_params)
+
+        max_distance=0
+        camera_movement_x, camera_movement_y = 0, 0
+        for i, (new,old) in enumerate(new_features, old_features):
+            new_features_point = new.ravel()
+            old_features_point = old.ravel()
+
+            distance = measure_distance(new_features_point, old_features_point)
+            if distance > max_distance:
+                max_distance = distance
+                camera_movement_x, camera_movement_y = measure_xy_distance(old_features_point, new_features_point)
+        
+            if max_distance > self.minin
